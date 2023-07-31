ce124cd29ef2f9ecad01c7c436a2cac3b6a0f59f
MariDantas <61691909+MariDantas@users.noreply.github.com>
2023-07-31 18:26:33 -0300

    Delete Text Ellipsis Project I directory

--- Text Ellipsis Project I/helpers.js
+++
@@ -1,6 +0,0 @@
-/**
- * @param {string} text
- */
-export function getDescription(text) {
-    return text.substring(0, 10);
-}
--- Text Ellipsis Project I/index.js
+++
@@ -1,8 +0,0 @@
-import {getDescription} from "./helpers.js";
-
-const input = document.querySelector("#input");
-const output = document.querySelector("#output");
-
-input.addEventListener("input", (event) => {
-    output.textContent = getDescription(event.currentTarget.value);
-});
